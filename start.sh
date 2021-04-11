d9bd982365b39d228c4796ba1ad350c2731b2fed
2838778326 <52909533+2838778326@users.noreply.github.com>
2021-04-11 16:40:08 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 a2dismod mpm_event
-sed -i "s/Listen 8080/Listen ${PORT:-8080}/g" /etc/apache2/ports.conf
+sed -i "s/Listen 80/Listen ${PORT:-880}/g" /etc/apache2/ports.conf
 apache2-foreground "$@"
